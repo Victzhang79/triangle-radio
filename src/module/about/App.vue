74e461f2bc5e9296dcb63cddb38c2c38cda13876
zhangyongming <zishixiuwen@126.com>
2018-10-21 18:20:46 +0800

    feat:about page

--- src/module/about/App.vue
+++ src/module/about/App.vue
@@ -1,6 +1,5 @@
 <template>
 	<div id="app">
-		<img src="./assets/logo.png">
 		<router-view></router-view>
 	</div>
 </template>
@@ -11,13 +10,14 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+@import '../../assets/style/public.scss';
 #app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	font-family: PingFangSC-Regular, Microsoft YaHei, tahoma, arial, 'Avenir',
+		Helvetica, \5b8b\4f53, sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
 	text-align: center;
 	color: #2c3e50;
-	margin-top: 60px;
 }
 </style>
