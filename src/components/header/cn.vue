b09016fce732976a431771bf7e3dda5833db70a7
zhangyongming <zishixiuwen@126.com>
2018-10-22 19:56:43 +0800

    feat: menu active style

--- src/components/header/cn.vue
+++ src/components/header/cn.vue
@@ -5,24 +5,24 @@
 				<img class="list-btn" src="../../assets/imgs/hiden_list.png" alt="展开">
 				<transition name="slide">
 					<ul v-show="hideMenuVisible" class="hide-list">
-						<li>
+						<li :class="{active: activePage === 'about'}">
 							<a :href="HOST+'/m/about'">关于我们</a>
 						</li>
-						<li>
+						<li :class="{active: activePage === 'usercenter'}">
 							<a :href="HOST+(isLog?'/m/userCenter':'/m/userEntry')">用户中心</a>
 						</li>
 						<li @click="logOut" v-if="isLog">退出登录</li>
 					</ul>
 				</transition>
 			</li>
-			<li>
+			<li :class="{active: activePage === 'pawn'}">
 				<a :href="HOST+'/m/pawn'">典当</a>
 			</li>
-			<li>
+			<li :class="{active: activePage === 'coins'}">
 				<a :href="HOST+'/m/coins'">币生币</a>
 			</li>
-			<li>
-				<a :href="HOST+'/m'" class="active">主页</a>
+			<li :class="{active: activePage === 'home'}">
+				<a :href="HOST+'/m'">主页</a>
 			</li>
 		</ul>
 	</top-menu>
