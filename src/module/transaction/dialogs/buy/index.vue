b7ed4e08b2a792d8844e96c1e8faa0a15cec4516
zhangyongming <zishixiuwen@126.com>
2018-11-04 17:53:27 +0800

    feat:交易页&购买弹窗

--- src/module/transaction/dialogs/buy/index.vue
+++ src/module/transaction/dialogs/buy/index.vue
@@ -1,23 +1,26 @@
 <template>
 	<div>
-		<el-dialog class="dialog buy-dialog" width="520px" title="" :visible.sync="visible">
+		<van-popup class="dialog buy-dialog" v-model="visible">
+			<div @click="visible=false" class="close"></div>
 			<h3 class="dlg-title">购买AOK</h3>
-			<el-form inline label-position="right" style="margin-top: 30px;" label-width="120px" class="wrap">
-				<el-form-item label="买入价：">
-					<el-input-number class="input" :step="priceStep" v-model="buyPrice">
-					</el-input-number>
-				</el-form-item>
-				<span style="line-height: 40px">{{coins[payCoin]}}</span>
-				<el-form-item label="买入数量：">
-					<el-input-number class="input" v-model="buyNum" :min="100" :max="100000">
-					</el-input-number>
-				</el-form-item>
-			</el-form>
-			<p class="info">需要{{needPrice}}{{coins[payCoin]}}，可用余额为{{balance}}{{coins[payCoin]}}，<span @click="goRechage" class="rechage">充值</span></p>
+			<div class="wrap">
+				<label>
+					<span class="labelTit">买入价：</span>
+					<input v-validate="rules.buyPriceRule.validation" type="number" class="shortInput" :step="priceStep" v-model="buyPrice" width="50%" name="buyPrice">
+					<span>{{coins[payCoin]}}</span>
+					<p v-show="errors.has('buyPrice')" class="error-tip">{{ rules.buyPriceRule.text}}</p>
+				</label>
+				<label>
+					<span class="labelTit">买入数量：</span>
+					<input v-validate="rules.buyNumRule.validation" type="text" class="shortInput" v-model="buyNum" name="buyNum">
+					<p v-show="errors.has('buyNum')" class="error-tip">{{ rules.buyNumRule.text}}</p>
+				</label>
+			</div>
+			<p class="info">需要{{needPriceShow}}{{coins[payCoin]}}，可用余额为{{balanceShow}}{{coins[payCoin]}}，<span @click="goRechage" class="rechage">充值</span></p>
 			<p class="btn-line">
-				<el-button @click="buy" class="btn">购买</el-button>
+				<button @click="buy" class="btn">购买</button>
 			</p>
-		</el-dialog>
+		</van-popup>
 		<box-charge v-model="boxRechageVisible" :item="boxRechargeItem"></box-charge>
 	</div>
 
@@ -37,14 +40,36 @@ export default {
 			},
 			set(value) {
 				this.$store.commit('changeBuyDlgVisible', value);
+				if (!value) {
+					this.$store.commit('commitBuyInfo', {
+						payCoin: 0,
+						price: 0
+					});
+				}
 			}
 		},
 		needPrice() {
-			return this.price * this.buyNum;
+			return this.buyPrice * this.buyNum;
 		},
 		priceStep() {
-			let precision = this.price.toString().split('.')[1];
+			let precision = this.buyPrice.toString().split('.')[1];
 			return 1 / Math.pow(10, precision ? precision.length - 1 : 0);
+		},
+		needPriceShow() {
+			let precision = this.needPrice.toString().split('.')[1];
+			if (precision !== undefined && precision.length > 8) {
+				return this.needPrice.toFixed(8);
+			} else {
+				return this.needPrice.toString();
+			}
+		},
+		balanceShow() {
+			let precision = this.balance.toString().split('.')[1];
+			if (precision !== undefined && precision.length > 8) {
+				return this.balance.toFixed(8);
+			} else {
+				return this.balance.toString();
+			}
 		}
 	},
 	watch: {
@@ -52,9 +77,29 @@ export default {
 			this.buyPrice = value;
 		}
 	},
+	created() {
+		this.buyPrice = this.price;
+	},
 	data() {
 		return {
+			rules: {
+				buyPriceRule: {
+					validation: {
+						required: true
+					},
+					text: '购买价格不能为空'
+				},
+				buyNumRule: {
+					validation: {
+						required: true,
+						min_value: '100',
+						max_value: '100000'
+					},
+					text: '100个起售，最多不超过100,000个'
+				}
+			},
 			coins: {
+				'0': '',
 				'1': 'USDT',
 				'2': 'BTC',
 				'3': 'ETH',
@@ -71,31 +116,32 @@ export default {
 	},
 	methods: {
 		goRechage() {
-			// window.location.href =
-			// 	window.location.protocol +
-			// 	'//' +
-			// 	window.location.host +
-			// 	'/userCenter';
 			this.visible = false;
 			this.boxRechargeItem = {
 				coinCode: this.payCoin,
 				walletAddr: this.walletAddr
 			};
-			this.boxRechageVisible = true;
+			setTimeout(() => {
+				this.boxRechageVisible = true;
+			}, 300);
 		},
 		buy() {
-			submitOrder(this.payCoin, this.buyPrice, this.buyNum)
-				.then(data => {
-					if (data.code === 200) {
-						this.$message.success(data.data);
-						this.visible = false;
-					} else {
-						this.$message.error(data.msg);
-					}
-				})
-				.catch(err => {
-					console.log('error:', err);
-				});
+			this.$validator.validateAll().then(result => {
+				if (result) {
+					submitOrder(this.payCoin, this.buyPrice, this.buyNum)
+						.then(data => {
+							if (data.code === 200) {
+								this.$toast.success(data.data);
+								this.visible = false;
+							} else {
+								this.$toast.fail(data.msg);
+							}
+						})
+						.catch(err => {
+							console.log('error:', err);
+						});
+				}
+			});
 		}
 	},
 	components: {
@@ -105,6 +151,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import '../../assets/css/dialog.scss';
+@import '@/assets/style/dialog.scss';
 @import './index.scss';
 </style>
--- src/module/transaction/pages/transaction/index.vue
+++ src/module/transaction/pages/transaction/index.vue
@@ -1,54 +1,41 @@
 <template>
 	<div class="transaction-page">
 		<div class="banner">
-			<h1 class="tit">KOOBANK克班克——数字资产管理集团-投资银行</h1>
+			<h1 class="tit">基金理财—跨越牛熊</h1>
 			<ul class="list">
-				<li class="item">* 由前VISA、Standard Chartered Bank、JPMorgan、Microsoft、VCSIG等高管联合创建。</li>
-				<li class="item">* 团队具有超过十年以上金融证券投资、家族财富管理、私募股权投资经验。</li>
-				<li class="item">* 聚焦数字资产配置与管理，去中心化的数字资产管理未来解决方案。</li>
+				<li class="item">* 成熟团队确保高收益低风险。</li>
+				<li class="item">* 冷钱包隔离确保资金安全。</li>
+				<li class="item">* 冷钱包隔离确保资金安全。</li>
 			</ul>
 		</div>
 		<div class="mainArea">
 			<div class="header">
 				<p class="tit">主区</p>
-				<p class="note">交易价格来自天天交易所，<a class="link" href="https://www.ttex.com/" target="_blank">点击查看</a></p>
+				<p class="note">交易价格来自天天交易所，一分钟更新一次，<a class="link" href="https://www.ttex.com/" target="_blank">点击查看</a></p>
 			</div>
 			<div class="table">
-				<div class="table-head">
-					<span>币种</span>
-					<span>价格</span>
-					<span>最高</span>
-					<span>最低</span>
-					<span>24H成交量</span>
-					<span>操作</span>
-				</div>
-				<div class="table-body">
-					<div v-for="(item, index) in transPriceList" :key="index" class="groupItem">
-						<div class="item">
-							<img class="icon" :src="coins[item.coinCode].imgPath">
-							AOK/{{coins[item.coinCode].name}}
-						</div>
-						<div class="item downFluctuation">{{item.last}}</div>
-						<div class="item">{{item.high}}</div>
-						<div class="item">{{item.low}}</div>
-						<div class="item">{{item.vol}}</div>
-						<div class="item">
-							<el-button @click="buy(item)" size="small" style="background-color:#2ab3ce;color: #fff;border-color:#2ab3ce;" round>购买</el-button>
-							<el-button @click="showHistory(item.coinCode)" size="small" round>委托记录</el-button>
-						</div>
+				<div v-for="(item, index) in transPriceList" :key="index" class="groupItem">
+					<div class="firstLine">
+						<p class="coin">AOK<span class="coinTxt">/{{coins[item.coinCode].name}}</span></p>
+						<p class="price">¥{{item.last}}</p>
+						<p class="buy"><button @click="buy(item)">购买</button></p>
 					</div>
+					<div class="secondLine">
+						<p class="transNum">24H成交量：{{item.vol}}</p>
+						<p class="history"><button @click="showHistory(item.coinCode)">委托记录</button></p>
+					</div>
+					<!-- <p class="item">{{item.high}}</p> -->
+					<!-- <p class="item">{{item.low}}</p> -->
 				</div>
 			</div>
 		</div>
 		<buy-dialog></buy-dialog>
-		<!-- <history v-model="historyVisible" :coinCode="historyCoinCode"></history> -->
 	</div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
 import BuyDialog from '../../dialogs/buy';
-// import History from '../../dialogs/history';
 export default {
 	computed: {
 		...mapGetters(['transPriceList'])
@@ -99,13 +86,13 @@ export default {
 			this.$store.commit('changeBuyDlgVisible', true);
 		},
 		showHistory(coinCode) {
-			this.historyCoinCode = coinCode;
-			this.historyVisible = true;
+			// this.historyCoinCode = coinCode;
+			// this.historyVisible = true;
+			this.$router.push('/history/' + coinCode);
 		}
 	},
 	components: {
-		BuyDialog,
-		History
+		BuyDialog
 	}
 };
 </script>
