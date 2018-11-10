02c5e541de377dd1787a854dab5fc962697d1869
Alex <875877496@qq.com>
2018-11-10 14:42:57 +0800

    Fix:压缩图片

--- src/components/identityAuth/index.vue
+++ src/components/identityAuth/index.vue
@@ -4,7 +4,7 @@
 		<h2 class="dialog-title">身份认证</h2>
 		<div class="dialog-body">
 			<div class="sub-title">{{actionName}}前请先进行身份认证</div>
-			<img src="//www.koobank.com/m/img/user_head.png" class="user-head" />
+			<img src="//www.trxwallet.net/img/user_head.png" class="user-head" />
 		</div>
 		<div class="dialog-footer">
 			<div class="submit-btn" @click="submit">立即身份认证</div>
