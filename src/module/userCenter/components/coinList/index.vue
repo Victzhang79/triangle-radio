b81cab684121e8134d20b85e122142514d28c54f
Alex <875877496@qq.com>
2018-12-07 13:12:18 +0800

    Fix：打开充提功能

--- src/module/userCenter/components/coinList/index.vue
+++ src/module/userCenter/components/coinList/index.vue
@@ -266,19 +266,19 @@ export default {
     },
     // 充值
     recharge(item) {
-      this.$toast({
-        message: "波场主网升级，暂停充提",
-        duration: this.duration
-      });
-      //   if (item.coinCode == 1 || item.coinCode == 2) {
-      //     this.$toast({
-      //       message: "转账人数过多，请稍后再试",
-      //       duration: this.duration
-      //     });
-      //   } else {
-      //     this.checkItem = item;
-      //     this.showRechargeBox = true;
-      //   }
+      //   this.$toast({
+      //     message: "波场主网升级，暂停充提",
+      //     duration: this.duration
+      //   });
+      if (item.coinCode == 1 || item.coinCode == 2) {
+        this.$toast({
+          message: "转账人数过多，请稍后再试",
+          duration: this.duration
+        });
+      } else {
+        this.checkItem = item;
+        this.showRechargeBox = true;
+      }
     },
     deposit(item) {
       // this.$toast({
